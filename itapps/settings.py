c86e163e7ed6164b1bbd138d9ef4249aee1fa0f3
rascove <rascove@yahoo.com>
2023-03-06 00:58:33 +0000

    Retry with new settings

--- itapps/settings.py
+++ itapps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = os.environ['WEBSITE_HOSTNAME']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 
 
 # Application definition
